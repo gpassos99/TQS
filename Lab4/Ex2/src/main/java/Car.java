3f872efa6a4bb08f059e801d7a8783fed7166593
Passos <gpassos99>
2021-04-16 11:33:24 +0100

    Revert "Lab4"
    This reverts commit 77bf8bcc1409e5c27a800c11bcae69cffd1c469a.

---
+++ Lab4/Ex2/src/main/java/Car.java
@@ -0,0 +1,49 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String maker;
+
+    public Car(String name, String maker) {
+        this.name = name;
+        this.maker = maker;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+    public void setMaker(String maker) {
+        this.maker = maker;
+    }
+
+
+}
